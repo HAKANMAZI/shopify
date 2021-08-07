40d45b0b48d645a8fd61581d8d9b69ddd921e0eb
HAKANMAZI <hak122hak@gmail.com>
2021-08-07 16:19:30 +0300

    yeni

--- hakanscrapeShopify.py
+++ hakanscrapeShopify.py
@@ -7,6 +7,62 @@ import html2text
 h = html2text.HTML2Text()
 h.ignore_links = True
 
+class TekShopifyScrape():
+    def __init__(self) -> None:
+        self.product_list =  []
+        self.count = 0
+
+    def extractWebPages(self, url):
+        r = requests.get(url)
+        data = r.json()
+
+        for item in data['products']:
+            title = item['title']
+            handle = item["handle"]
+            body_html = h.handle(item['body_html'])
+            saf_body = item['body_html']
+            product_type = item['product_type']
+            published_at = item['published_at']
+            created_at = item['created_at']
+            updated_at = item['updated_at']
+            product_url = "https://completedworks.com" + '/products/' + item['handle']
+
+            for image in item['images']:
+                try:
+                    imagesrc = image['src']
+                except:
+                    imagesrc = 'None'
+
+            for variant in item['variants']:
+                price = variant['price']
+                sku = variant['sku']
+                available = variant['available']
+                print(price, available)
+
+                product  = {
+                    'web site' : url[:-32],
+                    'title' : title,
+                    'handle' : handle,
+                    'body_html': body_html,
+                    'saf_body': saf_body,
+                    'published_at' : published_at,
+                    'updated_at' : updated_at,
+                    'created_at' : created_at,
+                    'product_type' : product_type,
+                    'price' : price,
+                    'sku' : sku,
+                    'available' : available,
+                    'image' : imagesrc,
+                    'product_url' : product_url
+                    
+                }
+
+                self.product_list.append(product)
+
+    def save_csv(self):
+        df = pd.DataFrame(self.product_list)
+        df.to_csv("product_list_test.csv")
+
 
 class Cshopify():
     def __init__(self) -> None:
@@ -119,10 +175,12 @@ class Cshopify():
         #print(soup.find('table', { 'class' : 'table' }).text)
 
 if __name__=="__main__":
-    cls = Cshopify()
-
-    cls.extractWebPages("https://completedworks.com/products.json?limit=10000#page=1")
-    cls.extractWebPages("https://tortware.com/products.json?limit=10000#page=1")
-    cls.save_csv()
-
+    #cls = Cshopify()
     #cls.dongu()
+
+    tekcls = TekShopifyScrape()
+    tekcls.extractWebPages("https://completedworks.com/products.json?limit=10000#page=1")
+    #tekcls.extractWebPages("https://tortware.com/products.json?limit=10000#page=1")
+    tekcls.save_csv()
+
+    
